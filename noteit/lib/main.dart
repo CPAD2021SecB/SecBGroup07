a02ab5d8c63b94d9a15476339ace4460f744193b
Nishant Nimbalkar <nimbalkarnishant98@gmail.com>
2021-12-09 18:20:21 +0530

    Added Login with Google

--- noteit/lib/main.dart
+++ noteit/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:noteit/pages/login.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
       title: 'NOTEIT',
       theme: ThemeData.dark().copyWith(
         primaryColor: Colors.white,
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
       ),
       home: LoginPage(),
     );
